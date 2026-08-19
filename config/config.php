e17df5b48f77de4852b6816c244673b6d2e3e431
Ronaldo <ronaldoldiniz@users.noreply.github.com>
2026-08-19 06:37:52 -0300

    fix: restaurar config.php no Git para implantacao direta

---
+++ config/config.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Arquivo de configuração ativo da API Gestão EPI.
+ * Contém credenciais de banco e chaves de segurança fixadas.
+ */
+
+return [
+    'db' => [
+        'host' => 'db-gestao-epi-gestaoepi.a.aivencloud.com',
+        'port' => '10903',
+        'dbname' => 'defaultdb',
+        'username' => 'avnadmin',
+        'password' => 'AVNS_T2WnhU7_Df8MJ2CvuW0',
+        'charset' => 'utf8mb4'
+    ],
+    'app' => [
+        'env' => 'production',
+        'debug' => false,
+        'secret_key' => 'gestao_epi_api_chavesecretadoservidor_987654321',
+        'token_ttl' => 43200
+    ],
+    'security' => [
+        'max_login_attempts' => 5,
+        'lockout_time' => 900,
+        'max_pin_attempts' => 3,
+        'aes_key' => '89b856ebf864d56bf299c51355bac0b3f9028f00c50b7ddebe6cbe4936cfb85f',
+        'hmac_key' => 'ff949f4043290a78fb87feb34410a57fc6e6c0db8df39513edaf635920487c7c'
+    ]
+];
